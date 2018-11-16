e97a005cc9c1bb1fe8fec56fe878e2fb978f4dd3
Danilo de Oliveira <doliveira.doti@bancomaxima.com.br>
2018-11-16 14:31:39 -0200

    Criando a página de registro

---
+++ src/ByteBank.Forum/Controllers/ContaController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using ByteBank.Forum.ViewModels;
+
+namespace ByteBank.Forum.Controllers
+{
+    public class ContaController : Controller
+    {
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registrar(ContaRegistrarViewModel modelo)
+        {
+            return View();
+        }
+    }
+}
